a061a9c1ee00b1233126395183bad5dc4c5d81fd
nimfadora2 <kislowik95@gmail.com>
2018-10-17 14:57:47 +0200

    Image entropy calculation
    First step of preparing the project. Calculating the entropy of image based on two dimensions histogram with given parametres s and t.

---
+++ main.py
@@ -0,0 +1,69 @@
+import cv2
+import numpy as np
+
+A = cv2.imread('lena.png',0)
+
+### Function to calculate neighbours on image - rowe 0, n-1 are omitted ###
+def neighbors(image):
+	width, height = image.shape
+	neighs = []
+	for i in range(1, width - 1):
+		current_row = []
+		for j in range(1, height - 1):
+			current_neigh = np.sum(image[i-1:i+2,j-1:j+2])//9
+			current_row.append(current_neigh)
+		neighs.append(current_row)
+	return neighs
+
+### Function to calculate two dimensional histogram based on image and neighs table ###
+def hist2D(image,neighs):
+	width, height = image.shape
+	hist = [[0]*255]*255
+
+	for i in range(1, width - 1):
+		for j in range(1, height - 1):
+			hist[image[i,j]][neighs[i-1][j-1]]+= 1
+
+	return hist
+
+### Function to calculate probability mass function basen on histogram ###
+def probab(hist, size):
+	prob = []
+	for row in hist:
+		prob.append([x/size for x in row])
+	return prob
+
+### Function to calculate probability distribution to first and second area ###
+def probDistr(prob,s,t):
+	return np.sum(prob[0:s][0:t]),np.sum(prob[s:][t:])
+
+### Function to calculate dicrete entropy of both areas ###
+def discrEntr(prob,s,t):
+	H1 = 0
+	H2 = 0
+	for row in prob[:s]:
+		for elem in row[:t]:
+			if elem != 0:
+				H1 += elem*np.log(elem)
+	for row in prob[s:]:
+		for elem in row[t:]:
+			if elem != 0:
+				H2 += elem*np.log(elem)
+	return -H1,-H2
+
+### Function to calculate entropy of area ###
+def entropy(H,P):
+	return np.log(P) + H/P
+
+### Function to calculate phi on given s and t ###
+def phi(image,s,t):
+	neighs = neighbors(image)
+	hist = hist2D(image, neighs)
+	prob = probab(hist, image.size)
+	P1, P2 = probDistr(prob, s, t)
+	H1, H2 = discrEntr(prob, s, t)
+	H_1 = entropy(H1, P1)
+	H_2 = entropy(H2, P2)
+
+	return H_1 + H_2
+
